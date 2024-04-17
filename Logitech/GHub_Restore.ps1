a45645cd179baaaf1780d909d4a2529f5c44647b
Jose Olvera <armando.ob@hotmail.com>
2024-04-16 23:10:42 -0600

    Updated Restore

--- Logitech/GHub_Restore.ps1
+++ Logitech/GHub_Restore.ps1
@@ -1,11 +1,15 @@
 # Define the destination folder path
-$sourceFolder1 = "$PSScriptRoot\LGHUB"
+$sourceFolder1 = "$PSScriptRoot\Local\LGHUB"
+$sourceFolder2 = "$PSScriptRoot\Roaming\G HUB"
+$sourceFolder3 = "$PSScriptRoot\Roaming\lghub"
 $destinationFolder1 = "$env:LOCALAPPDATA\LGHUB"
-$sourceFolder2 = "$PSScriptRoot\G HUB"
 $destinationFolder2 = "$env:APPDATA\G HUB"
+$destinationFolder3 = "$env:APPDATA\lghub"
+
 
 # Copy a folder from sourceFolder1 to the destinationFolder1
 Copy-Item -Path $sourceFolder1 -Destination $destinationFolder1 -Recurse -Force
 
 # Copy a folder from sourceFolder2 to the destinationFolder2
 Copy-Item -Path $sourceFolder2 -Destination $destinationFolder2 -Recurse -Force
+Copy-Item -Path $sourceFolder3 -Destination $destinationFolder3 -Recurse -Force
