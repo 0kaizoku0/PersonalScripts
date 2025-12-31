a0d160ad6e8924a087208d195faf17fd7ce7953a
Jose Olvera <armando.ob@hotmail.com>
2025-12-31 00:11:35 -0600

    Captura compara dos videos

--- Video/CapturaFFMPEG.ps1
+++ Video/CapturaFFMPEG.ps1
@@ -1,7 +1,26 @@
-$video = "video.mp4"
+$video1 = "D:\Torrents\One Piece - 001 (DVDRip 768x576 x264 10bit AC3).mkv" # Pega la ruta con comillas
+$video2 = "D:\Torrents\One Piece\01 Saga de East Blue (001-061)\One Piece Opening 01 [Shichibukai] [53FB0BDE].mkv" # Pega la ruta con comillas
 $outputFolder = "screenshots"
 
 New-Item -ItemType Directory -Force -Path $outputFolder
 
-# Extrae 1 captura por cada minuto
-ffmpeg -i $video -vf fps=1/60 "$outputFolder\thumb_%04d.png"
+# Extrae 1 captura por cada minuto 1/60 o cada 10 segundos 1/10
+# ffmpeg -i $video1 -vf fps=1/10 "$outputFolder\thumb_%04d_video1.png"
+# ffmpeg -i $video2 -vf fps=1/10 "$outputFolder\thumb_%04d_video2.png"
+
+# ffmpeg -ss 00:10:00 -i $video1 -frames:v 1 -accurate_seek "$outputFolder\capture1.png"
+# ffmpeg -ss 00:10:00 -i $video2 -frames:v 1 -accurate_seek "$outputFolder\capture2.png"
+
+# Definir los timestamps
+$timestamps = @(
+    "00:00:30",
+    "00:01:00",
+    "00:01:00",
+    "00:05:00",
+    "00:10:00"
+)
+
+foreach ($t in $timestamps) {
+    ffmpeg -ss $t -i $video1 -frames:v 1 "$outputFolder\$($t.Replace(':','-'))_Video1.png"
+    ffmpeg -ss $t -i $video2  -frames:v 1 "$outputFolder\$($t.Replace(':','-'))_Video2.png"
+}
