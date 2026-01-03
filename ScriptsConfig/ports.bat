5e0f89992b8007231dac9b599d1eeee439a02617
Jose Olvera <armando.ob@hotmail.com>
2026-01-02 21:35:04 -0600

    Ports to 45490

--- ScriptsConfig/ports.bat
+++ ScriptsConfig/ports.bat
@@ -1,8 +1,8 @@
-netsh advfirewall firewall add rule name="uTorrent TCP 45483" ^
-dir=in action=allow protocol=TCP localport=45483
-netsh advfirewall firewall add rule name="uTorrent UDP 45483" ^
-dir=in action=allow protocol=UDP localport=45483
+netsh advfirewall firewall add rule name="uTorrent TCP 45490" ^
+dir=in action=allow protocol=TCP localport=45490
+netsh advfirewall firewall add rule name="uTorrent UDP 45490" ^
+dir=in action=allow protocol=UDP localport=45490
 
-netsh advfirewall firewall show rule name=all | findstr 45483
+netsh advfirewall firewall show rule name=all | findstr 45490
 
-netstat -ano | findstr 45483
+netstat -ano | findstr 45490
